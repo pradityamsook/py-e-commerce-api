c527851f5ad172f84b7899d71fb3522e984de843
Pradit Yamsook <pradit.y@ku.th>
2023-03-06 21:29:40 +0700

    create sql config in config's directory

---
+++ src/config/sql.config.ts
@@ -0,0 +1,41 @@
+interface typeConfig {
+    user: string;
+    password: string;
+    database: string;
+    server: string;
+    pool: {
+        max: number;
+        min: number;
+        idleTimeoutMillis: number
+    };
+    options: {
+        encrypt: boolean;
+        trustServerCertificate: boolean;
+    }
+}
+
+class Config {
+    sqlConfig() {
+        const config = {
+            user: process.env.USER_NAME,
+            password: process.env.PASSWORD,
+            database: process.env.DB_NAME,
+            server: process.env.SERVER?.toString(),
+            pool: {
+                max: 10,
+                min: 0,
+                idleTimeoutMillis: 30000
+            },
+            options: {
+                encrypt: true,
+                trustServerCertificate: true
+            }
+        } as typeConfig;
+    
+        return config;   
+    }
+}
+
+export {
+    Config
+}
