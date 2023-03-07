1aa98cc68f41b2827e37a9200aa1fbde3a6f4f2d
Pradit Yamsook <pradit.y@ku.th>
2023-03-07 22:46:22 +0700

    add contructor mapper

--- src/modules/authenticaion/services/authenticate.service.ts
+++ src/modules/authenticaion/services/authenticate.service.ts
@@ -1,14 +1,17 @@
 import { Request, Response } from "express";
 import { User } from "../interface/user.interface";
+import { AuthenticateMapper } from "../mapper/authenticate.mapper";
 import { AuthenticateRepository } from "../repository/authentivate.repository";
 
 export class AuthenticateService {
     constructor (
-        private readonly authenRepository: AuthenticateRepository = new AuthenticateRepository()
+        private readonly authenRepository: AuthenticateRepository = new AuthenticateRepository(),
+        private readonly authenMapper: AuthenticateMapper = new AuthenticateMapper()
     ) {};
 
     public async login(req: User): Promise<any> {
-        const result = await this.authenRepository.login(req);
+        let result = await this.authenRepository.login(req);
+
         return result;
     }
 }
