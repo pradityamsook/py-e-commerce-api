14717c541b2f9431ac5af4f1db79448b9a0c5744
Pradit Yamsook <pradit.y@ku.th>
2023-03-07 22:45:39 +0700

    add GetUser interface

--- src/modules/authenticaion/interface/user.interface.ts
+++ src/modules/authenticaion/interface/user.interface.ts
@@ -2,3 +2,10 @@ export interface User {
     username: string;
     password: string;
 }
+
+export interface GetUser {
+    username: string;
+    first_name: string;
+    last_name: string;
+    role: number;
+}
